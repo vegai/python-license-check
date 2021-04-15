bbcef3cd6d716407b3b38d594e6d0749150e81c0
Vesa Kaihlavirta <vesa.kaihlavirta@aiwo.ai>
2021-04-15 15:16:25 +0300

    These funcs not needed here

--- liccheck/poetry_lock.py
+++ liccheck/poetry_lock.py
@@ -10,18 +10,3 @@ def parse_poetry_lock(poetry_lock_file):
         requirements.append(pkg_resources.Requirement.parse(pkg_name))
 
     return requirements
-
-
-def resolve_without_deps(requirements):
-    working_set = pkg_resources.working_set
-    for req in requirements:
-        env = pkg_resources.Environment(working_set.entries)
-        dist = env.best_match(
-            req=req, working_set=working_set, installer=None, replace_conflicting=False,
-        )
-        yield dist
-
-
-def resolve(requirements):
-    for dist in pkg_resources.working_set.resolve(requirements):
-        yield dist
